2f70d732a74b3eb1976083074fe76d5b0e5107e8
Maggie Lin <linym012@163.com>
2017-06-03 19:25:24 -0400

    add AuthToken to ReportWriter Dashboard

--- Code/ReportWriter-master/ReportWriter/settings.py
+++ Code/ReportWriter-master/ReportWriter/settings.py
@@ -71,6 +71,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
     'report_mailer',
     'user_profile',
     'mailer',
